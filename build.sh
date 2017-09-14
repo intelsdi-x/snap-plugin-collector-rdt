44f9d1a37aa6927f2ce85c2e66f7acb2f3c798fc
Rafal Borysionek <boryseczek606@gmail.com>
2017-09-14 12:41:47 +0200

    Testing build on vagrant

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+export PATH=/opt/rh/devtoolset-3/root/usr/bin/:$PATH
+gcc --version
 mkdir -p build
 cd build
 cmake ..
--- install_deps_centos.sh
+++ install_deps_centos.sh
@@ -18,32 +18,18 @@ set -e
 
 mkdir -p lib
 
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
+# export CC=/usr/bin/gcc
+# export CXX=/usr/bin/g++
+
+export CC=/opt/rh/devtoolset-3/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-3/root/usr/bin/g++
+
+gcc --version
 
 pushd `pwd`
 cd third_party/intel-cmt-cat/lib/
 make SHARED=n
 cp libpqos.a ../../../lib/
-popd
-
-if [ ! -d "./third_party/protobuf/gmock" ]
-then
-pushd `pwd`
-cd ./third_party/protobuf
-wget http://pkgs.fedoraproject.org/repo/pkgs/gmock/gmock-1.7.0.zip/073b984d8798ea1594f5e44d85b20d66/gmock-1.7.0.zip
-unzip -q gmock-1.7.0.zip
-rm gmock-1.7.0.zip
-mv gmock-1.7.0 gmock
-popd
-fi
-
-pushd `pwd`
-cd ./third_party/protobuf
-./autogen.sh
-./configure
-make -j2
-make install
 popd
 
 pushd `pwd`
@@ -52,10 +38,25 @@ git checkout tags/v1.0.1
 git submodule update --init
 make -j2
 make install
+pushd `pwd`
+cd ./third_party/protobuf
+make clean
+./autogen.sh
+./configure
+make -j2
+make install 
+popd
 popd
 
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+pushd `pwd`
+cd ./third_party/googletest
+cmake CMakeLists.txt
+make -j2
+make install
+cp googlemock/libgmock.a ../../lib
+cp googlemock/gtest/libgtest.a ../../lib
+popd
+echo PO GOOGLETEST
 
 pushd `pwd`
 cd ./third_party/snap-plugin-lib-cpp
@@ -68,12 +69,12 @@ cp build/lib/libsnap.a ../../lib
 popd
 
 pushd `pwd`
-cd ./third_party/googletest
-cmake CMakeLists.txt
-make -j2
-make install
-cp googlemock/libgmock.a ../../lib
-cp googlemock/gtest/libgtest.a ../../lib
+cd ./third_party
+git clone https://github.com/square/certstrap
+cd certstrap
+./build
+export PATH=$PATH:/opt/certstrap/bin
+popd
 popd
 
 # /usr/local/lib is usually not in LD_LIBRARY_PATH
