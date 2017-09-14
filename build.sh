d5da87b63be69302f1d6812b73b44ef42a832385
Rafal Borysionek <boryseczek606@gmail.com>
2017-09-14 15:46:51 +0200

    Again testing vagrant
    work in progress- testing

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export PATH=/opt/rh/devtoolset-3/root/usr/bin/:$PATH
+export CC=/opt/rh/devtoolset-4/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-4/root/usr/bin/g++
+export PATH=/opt/rh/devtoolset-4/root/usr/bin/:$PATH
 gcc --version
 mkdir -p build
 cd build
--- install_deps_centos.sh
+++ install_deps_centos.sh
@@ -18,11 +18,9 @@ set -e
 
 mkdir -p lib
 
-# export CC=/usr/bin/gcc
-# export CXX=/usr/bin/g++
-
-export CC=/opt/rh/devtoolset-3/root/usr/bin/gcc
-export CXX=/opt/rh/devtoolset-3/root/usr/bin/g++
+export CC=/opt/rh/devtoolset-4/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-4/root/usr/bin/g++
+export PATH=/opt/rh/devtoolset-4/root/usr/bin/:$PATH
 
 gcc --version
 
@@ -33,11 +31,21 @@ cp libpqos.a ../../../lib/
 popd
 
 pushd `pwd`
+cd ./third_party/googletest
+cmake CMakeLists.txt
+make -j2
+make install
+cp googlemock/libgmock.a ../../lib
+cp googlemock/gtest/libgtest.a ../../lib
+popd
+
+pushd `pwd`
 cd ./third_party/grpc
 git checkout tags/v1.0.1
 git submodule update --init
 make -j2
 make install
+
 pushd `pwd`
 cd ./third_party/protobuf
 make clean
@@ -49,14 +57,33 @@ popd
 popd
 
 pushd `pwd`
-cd ./third_party/googletest
-cmake CMakeLists.txt
-make -j2
-make install
-cp googlemock/libgmock.a ../../lib
-cp googlemock/gtest/libgtest.a ../../lib
+cd ./third_party
+wget -c 'http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download'
+tar xf download
+rm download
+cd ./boost_1_58_0
+./bootstrap.sh --show-libraries
+./bootstrap.sh
+./b2 install --prefix=/usr -j2
+popd 
+
+pushd `pwd`
+cd ./third_party
+git clone https://github.com/gabime/spdlog.git
+cd ./spdlog
+cp -r include/spdlog /usr/include/
 popd
-echo PO GOOGLETEST
+
+pushd `pwd`
+cd ./third_party
+wget http://downloads.cpp-netlib.org/0.11.2/cpp-netlib-0.11.2-final.tar.gz
+tar xf cpp-netlib-0.11.2-final.tar.gz
+rm cpp-netlib-0.11.2-final.tar.gz
+cd cpp-netlib-0.11.2-final/
+cmake .
+make -j2
+make install 
+popd
 
 pushd `pwd`
 cd ./third_party/snap-plugin-lib-cpp
@@ -68,15 +95,7 @@ make install
 cp build/lib/libsnap.a ../../lib
 popd
 
-pushd `pwd`
-cd ./third_party
-git clone https://github.com/square/certstrap
-cd certstrap
-./build
-export PATH=$PATH:/opt/certstrap/bin
-popd
-popd
-
 # /usr/local/lib is usually not in LD_LIBRARY_PATH
 #sudo cp /usr/local/lib/* /usr/lib
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
 ldconfig -v
